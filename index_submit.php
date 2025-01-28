671ba5fdad0a8358db29bdcd7240ef3db76c5f9d
Waranchai47 <waranchaipakinsee@gmail.com>
2025-01-28 15:17:57 +0700

    delete

--- index_submit.php
+++
@@ -1,6 +0,0 @@
-<h1> แม่สูตรคูณของตัวเลข:
-  <?php if(isset($_REQUEST['email'])){ echo $_REQUEST['email'];}?>
-</h1>
-<h1> detail :
-  <?php echo isset($_REQUEST['email']) ? $_REQUEST['detail'] : "";?>
-</h1>
